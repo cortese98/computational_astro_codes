b5503c2579a28bcd0e119156019441c180f3e3a0
cortese98 <97063649+cortese98@users.noreply.github.com>
2022-02-14 21:29:38 +0100

    Update particles_Petar.py

--- particles_Petar.py
+++ particles_Petar.py
@@ -9,7 +9,7 @@ t_p,n_p,n_p_bin=np.genfromtxt("total_mass_PeTar.txt",usecols=(0,3,4),unpack=True
 plt.xlabel("t [Myr]")
 plt.ylabel("N")
 plt.plot(t_p,n_p,color='blue',linewidth=3,label='PeTar total')
-plt.plot(t_p,n_p_bin,color='green',linewidth=3,label='Petar single+binaries')
+plt.plot(t_p,n_p_bin,color='red',linewidth=3,label='Petar single+binaries')
 plt.legend()
 plt.tight_layout()
 plt.show()
